52b3342e2df8085c4f6d13ecb84a0fc56d2d3839
Arin0421 <arin9804@naver.com>
2022-11-09 15:43:04 +0900

    Initial commit

---
+++ src/main/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplication.java
@@ -0,0 +1,13 @@
+package com.hibit.kusitms26tht3hibitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kusitms26thT3HibitBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kusitms26thT3HibitBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibit.kusitms26tht3hibitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kusitms26thT3HibitBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
