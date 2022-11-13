6b0aca87c7ca575beda447f6e893834913aed3ab
hyoung <ycat01@naver.com>
2022-11-14 03:16:39 +0900

    [FEAT] Swagger 기본 세팅

--- src/main/java/com/hibit/kusitms26tht3hibitback/controller/UserController.java
+++ src/main/java/com/hibit/kusitms26tht3hibitback/controller/UserController.java
@@ -2,12 +2,15 @@ package com.hibit.kusitms26tht3hibitback.controller;
 
 import com.hibit.kusitms26tht3hibitback.domain.User;
 import com.hibit.kusitms26tht3hibitback.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Tag(name="user", description = "회원가입/로그인 API")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -15,7 +18,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "sign-in/up", description = "회원가입 로그인")
     @RequestMapping(method = RequestMethod.POST, path = "/sign-up")
+    @PostMapping("/posts")
     public User register(@RequestBody User user) {
 
         return userService.insertUser(user);
