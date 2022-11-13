3d131ba4e8b1d7fd117fb0a81253b4f3c3d35b77
hyoung <ycat01@naver.com>
2022-11-13 17:43:46 +0900

    [FEAT] S3 setting

---
+++ src/main/java/com/hibit/kusitms26tht3hibitback/GettingStartedSpringAwsS3Application.java
@@ -0,0 +1,16 @@
+package com.hibit.kusitms26tht3hibitback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedSpringAwsS3Application {
+
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedSpringAwsS3Application.class, args);
+    }
+
+}
