41569b03a5eb529f779de124f5f7c6aedd62db56
hyoung <ycat01@naver.com>
2022-11-13 22:51:45 +0900

    [FIX] application.properties 추가

--- src/main/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplication.java
+++ src/main/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplication.java
@@ -2,8 +2,9 @@ package com.hibit.kusitms26tht3hibitback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
 public class Kusitms26thT3HibitBackApplication {
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
