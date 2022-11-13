e00aca67dcb5ec9c3139d2419a0038c8a13a4d28
hyoung <ycat01@naver.com>
2022-11-13 17:50:02 +0900

    [FIX] s3 application

--- src/main/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplication.java
+++ src/main/java/com/hibit/kusitms26tht3hibitback/Kusitms26thT3HibitBackApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Kusitms26thT3HibitBackApplication {
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Kusitms26thT3HibitBackApplication.class, args);
