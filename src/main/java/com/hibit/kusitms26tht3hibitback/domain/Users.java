9025a0d50280eeb1c90a177e16fe113971ad74b5
Arin0421 <arin9804@naver.com>
2022-11-16 14:41:49 +0900

    feat : 회원가입, 로그인 스웨거 설정 #5

--- src/main/java/com/hibit/kusitms26tht3hibitback/domain/Users.java
+++ src/main/java/com/hibit/kusitms26tht3hibitback/domain/Users.java
@@ -1,5 +1,6 @@
 package com.hibit.kusitms26tht3hibitback.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 @Data
 @Entity
 @Table(name = "users")
+@Schema(description = "회원")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Users implements UserDetails {
@@ -27,30 +29,38 @@ public class Users implements UserDetails {
     private int idx;
 
     @Column(nullable = false, length = 15)
+    @Schema(description = "아이디", example = "arin123")
     private String id;
 
     @Column(nullable = false, length = 20)
+    @Schema(description = "비밀번호", example = "1234")
     private String password;
 
     @Column(nullable = false, length = 20)
+    @Schema(description = "닉네임", example = "아린")
     private String nickname;
 
     @Column(nullable = false, length = 11)
+    @Schema(description = "전화번호", example = "01011112222")
     private String phone_number;
 
     @Column(nullable = false, length = 2)
+    @Schema(description = "나이", example = "24")
     private int age;
 
     @Column(nullable = false)
+    @Schema(description = "성별", example = "True")
     private boolean gender;
 
     @Column(nullable = true)
     private double temperature;
 
     @Column(nullable = false, length = 50)
+    @Schema(description = "거주지", example = "경기도")
     private String home;
 
     @Column(nullable = false, length = 100)
+    @Schema(description = "자기소개", example = "안녕")
     private String introduce;
 
     @Column
